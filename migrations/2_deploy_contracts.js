a74f97075492c0b618cc38afa4271f8f0bfbeaf3
Giorgos Petrou <gdpetrou@csd.auth.gr>
2018-11-07 15:47:43 +0200

    create papers in migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 var Papers = artifacts.require("./Papers.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Papers);
+  deployer.deploy(Papers).then(contract => {
+  	contract.createPaper(1, 'Sample Headline 1', 'Sample Title', 'Sample URL', 'Sample Description')
+    contract.createPaper(2, 'Sample Headline 2', 'Sample Title', 'Sample URL', 'Sample Description')
+    contract.createPaper(3, 'Sample Headline 3', 'Sample Title', 'Sample URL', 'Sample Description')
+    contract.createPaper(4, 'Sample Headline 4', 'Sample Title', 'Sample URL', 'Sample Description')
+    contract.createPaper(5, 'Sample Headline 5', 'Sample Title', 'Sample URL', 'Sample Description')
+  }) ;
 };
